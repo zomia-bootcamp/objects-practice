a92ac9288f1aa007c681aa2bdeedd527a0ac78c5
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-28 16:45:43 +0300

    Update 2_fasterSpaceship.js

--- Tests/2_fasterSpaceship.js
+++ Tests/2_fasterSpaceship.js
@@ -1,5 +1,5 @@
 // INPUT TYPE: Object
-// Instuctions: The object passed into the function has a key work speed. Increase the speed by 100 and return the function;
+// Instuctions: The object passed into the function has a key speed. Increase the speed by 100 and return the object;
 
 const fasterSpaceship = (obj) => {
     
